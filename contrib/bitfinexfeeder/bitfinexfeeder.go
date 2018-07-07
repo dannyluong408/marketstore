ab687bcc276b417da4b992fbde5bbbbde5166f5f
Danny <dannyluong408@gmail.com>
2018-07-07 12:55:48 -0700

    Dockerfile update

--- contrib/bitfinexfeeder/bitfinexfeeder.go
+++ contrib/bitfinexfeeder/bitfinexfeeder.go
@@ -205,7 +205,7 @@ func (bf *BitfinexFetcher) Run() {
 		timeEndM = timeEnd.UnixNano() / (int64(time.Millisecond) / int64(time.Nanosecond))
 
 		for _, symbol := range symbols {
-
+			glog.Infof("Start: %v, End: %v", timeStart.Unix(), timeEnd.Unix())
 			glog.Infof("Requesting %s %v - %v", bitfinex.TradingPrefix + symbol, timeStart, timeEnd)
 			rates, err := client.Candles.GetOHLCV(timeInterval, bitfinex.TradingPrefix + symbol, timeStartM, timeEndM)
 			if err != nil {
